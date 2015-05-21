fdbb7e2fe6c993a29435c1e84366d17c2bb5f519
liu_chang2010 <liu_chang2010@hotmail.com>
2015-05-21 11:30:22 -0400

    bootstrap added

---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void username_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void submitbutton_Click(object sender, EventArgs e)
+        {
+            username.Text = "";
+            username.Enabled = false;
+            password.Enabled = false;
+            instructionlabel.Text = "Thank you for logging in.";
+        }
+    }
+}
